f4088bd52208db1ae215e9a45c19dea5977463e2
tom ayers <tom ayers>
2016-08-17 18:49:43 -0400

    Initial commit

---
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
